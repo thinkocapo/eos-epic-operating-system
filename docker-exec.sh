dd0cee2b94f5d25b733f891f9cb0645bdd4d9f3f
thinkocapo <thinkocapo@gmail.com>
2018-07-04 18:34:41 +0300

    update again

--- docker-exec.sh
+++ docker-exec.sh
@@ -1,3 +1,4 @@
+# pay attention to eos.sh to see if this script is even being executed...
 containerid=$(docker ps -a | grep eos | awk '{print $1}')
 docker exec -it $containerid /bin/bash
 echo 're-run ./docker-exec.sh because container wasnt running yet...'
--- eos.sh
+++ eos.sh
@@ -1,23 +1,18 @@
 containerid=$(docker ps -a | grep eos | awk '{print $1}')
 
-#WORKS
-tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
-
-# WORKS
-# tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'docker exec -it '$containerid' /bin/bash'
+# 2 APPROACHES...
 
 
+# APPROACH 1 - enter the bash shell for EOS docker contaienr and work from there, but may have issue deploying/getting files?
 
+# WORKS - runs docker-exec.sh
+# tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
 
-# 2 APPROACHES
-
-
-#tmux new-session -t ethereum \; split-window -v -c ~/Projects/full-stack-fund \; send-keys -t 0 'ganache-cli testrpc' C-m \; send-keys -t 1 'node cli-app/setup-cli-original.js' \; new-window -c ~/Projects/full-stack-fund -d
-# 1
-# go into bash shell in docker container..... consider using a docker-exec.sh
-# exec -it <containerId> /bin/bash
+# WORKS - doesn't run docker-exec.sh
+tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'docker exec -it '$containerid' /bin/bash'
 
 
 
+#APPROACH 2... - this alias will enter the bash shell (docker container) every time and run a command, when it finishes, you'll still be left in your regular terminal/shell
 # 2
 # alias cleos (harder, must set this)...
