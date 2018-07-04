573f24aabda6c0317b04de2bbaa685be927e59c9
thinkocapo <thinkocapo@gmail.com>
2018-07-04 19:41:40 +0300

    Scripts, Tmux, finally the way I want them :)

--- docker-exec.sh
+++ docker-exec.sh
@@ -1,11 +1,16 @@
 # pay attention to eos.sh to see if this script is even being executed...
 containerid=$(docker ps -a | grep eos | awk '{print $1}')
-# docker exec -it $containerid /bin/bash
-# echo 're-run ./docker-exec.sh because container wasnt running yet...'
 
 echo 'SET THE FOLLOWING ALIAS...\n'
-# echo 'alias cleos='docker exec -it $containerid /opt/eosio/bin/cleos --url http://localhost:8888/''
+# WORKS
 echo 'alias cleos='"'"'docker exec -it '$containerid' /opt/eosio/bin/cleos --url http://localhost:8888'"'"
 
 echo '\nAND RUN THE FOLLOWING...\n'
 echo 'cleos get info'
+
+
+# OPTIONAL...comment out the above approach
+# Starts bash shell right away
+# containerid=$(docker ps -a | grep eos | awk '{print $1}')
+# docker exec -it $containerid /bin/bash
+# echo 're-run ./docker-exec.sh because container wasnt running yet...'
--- eos.sh
+++ eos.sh
@@ -1,22 +1,13 @@
 
 containerid=$(docker ps -a | grep eos | awk '{print $1}')
-# alias cleos='docker exec -it '$containerid' /opt/eosio/bin/cleos --url http://localhost:8888/'
 
+# WORKS - BEST July 4th, 2018
+tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'alias cleos='"'"'docker exec -it '$containerid' /opt/eosio/bin/cleos --url http://localhost:8888'"'" C-m \; send-keys -t 1 'cleos get info' C-m \;
 
-# APPROACH 1 - enter the bash shell for EOS docker contaienr and work from there, but may have issue deploying/getting files?
+# WORKS, docker-exec.sh doesn't try to run container before its ready (i.e. in the other pane) anymore
+# tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
 
-# WORKS, AND NOT RUNNING EXEC BEFORE CONTAINER IS READY ANYMORE, WHICH CAUSED ERROR.. - runs docker-exec.sh
-tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
-
-# WORKS BETTER - doesn't run docker-exec.sh
+# WORKS - gets use into bash shell for docker container. does not invoke docker-exec.sh
 # tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'docker exec -it '$containerid' /bin/bash'
 
-
-
-#APPROACH 2... - this alias will enter the bash shell (docker container) every time and run a command, when it finishes, you'll still be left in your regular terminal/shell
-# 2
-# alias cleos='docker exec -it '$f043bb1b25b6' /opt/eosio/bin/cleos --url http://localhost:8888/'
-
-
-
-echo 'THE END.....'
+echo 'Tmux EOS Session Killed...'
