0cc9a28f492a084a1c17ddaa2957f199f9567f41
thinkocapo <thinkocapo@gmail.com>
2018-07-04 19:25:30 +0300

    works

--- docker-exec.sh
+++ docker-exec.sh
@@ -1,4 +1,11 @@
 # pay attention to eos.sh to see if this script is even being executed...
 containerid=$(docker ps -a | grep eos | awk '{print $1}')
-docker exec -it $containerid /bin/bash
-echo 're-run ./docker-exec.sh because container wasnt running yet...'
+# docker exec -it $containerid /bin/bash
+# echo 're-run ./docker-exec.sh because container wasnt running yet...'
+
+echo 'SET THE FOLLOWING ALIAS...\n'
+# echo 'alias cleos='docker exec -it $containerid /opt/eosio/bin/cleos --url http://localhost:8888/''
+echo 'alias cleos='"'"'docker exec -it '$containerid' /opt/eosio/bin/cleos --url http://localhost:8888'"'"
+
+echo '\nAND RUN THE FOLLOWING...\n'
+echo 'cleos get info'
--- eos.sh
+++ eos.sh
@@ -1,18 +1,22 @@
-containerid=$(docker ps -a | grep eos | awk '{print $1}')
 
-# 2 APPROACHES...
+containerid=$(docker ps -a | grep eos | awk '{print $1}')
+# alias cleos='docker exec -it '$containerid' /opt/eosio/bin/cleos --url http://localhost:8888/'
 
 
 # APPROACH 1 - enter the bash shell for EOS docker contaienr and work from there, but may have issue deploying/getting files?
 
-# WORKS - runs docker-exec.sh
-# tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
+# WORKS, AND NOT RUNNING EXEC BEFORE CONTAINER IS READY ANYMORE, WHICH CAUSED ERROR.. - runs docker-exec.sh
+tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 './docker-exec.sh' C-m
 
-# WORKS - doesn't run docker-exec.sh
-tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'docker exec -it '$containerid' /bin/bash'
+# WORKS BETTER - doesn't run docker-exec.sh
+# tmux new-session -t eos \; split-window \; send-keys -t 0 './docker-start.sh' C-m \; send-keys -t 1 'docker exec -it '$containerid' /bin/bash'
 
 
 
 #APPROACH 2... - this alias will enter the bash shell (docker container) every time and run a command, when it finishes, you'll still be left in your regular terminal/shell
 # 2
-# alias cleos (harder, must set this)...
+# alias cleos='docker exec -it '$f043bb1b25b6' /opt/eosio/bin/cleos --url http://localhost:8888/'
+
+
+
+echo 'THE END.....'
